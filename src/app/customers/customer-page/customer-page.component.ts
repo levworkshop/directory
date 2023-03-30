0c6ce492a22ea59d8613d64cc1484bce1cb7bee7
levworkshop <noreply@githubusers.com>
2023-03-30 19:42:05 +0300

    customers data

--- src/app/customers/customer-page/customer-page.component.ts
+++ src/app/customers/customer-page/customer-page.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Customer {
+    name: string;
+    phone: string;
+    email: string;
+}
+
 @Component({
-  selector: 'app-customer-page',
-  templateUrl: './customer-page.component.html',
-  styleUrls: ['./customer-page.component.scss']
+    selector: 'app-customer-page',
+    templateUrl: './customer-page.component.html',
+    styleUrls: ['./customer-page.component.scss']
 })
 export class CustomerPageComponent {
-
+    customers: Array<Customer> = [
+        {
+            name: "Matilda",
+            phone: "039990000",
+            email: "matilda@email.com"
+        }, {
+            name: "mina",
+            phone: "025555555",
+            email: "mina@gmail.com",
+        },
+        {
+            name: "oliver",
+            phone: "025550000",
+            email: "twist@gmail.com"
+        }
+    ];
 }
