869aafd18dad26628599bb533bf2f195b2214dd3
levworkshop <noreply@githubusers.com>
2023-03-30 20:10:55 +0300

    form submit

--- src/app/customers/customer-page/customer-page.component.ts
+++ src/app/customers/customer-page/customer-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 interface Customer {
-    name: string;
-    phone: string;
-    email: string;
+    name?: string | null;
+    phone?: string | null;
+    email?: string | null;
 }
 
 @Component({
@@ -31,11 +32,35 @@ export class CustomerPageComponent {
         }
     ];
 
+    customerForm = new FormGroup({
+        name: new FormControl('', {
+            validators: Validators.required
+        }),
+        email: new FormControl('', {
+            validators: [
+                Validators.required,
+                Validators.email
+            ]
+        }),
+        phone: new FormControl('', {
+            validators: Validators.required
+        })
+    })
+
     customerTotal(): number {
         return this.customers ? this.customers.length : 0;
     }
 
     toggleForm() {
         this.showForm = !this.showForm;
+    }
+
+    onSubmit() {
+        if (!this.customerForm.valid) {
+            return;
+        }
+
+        this.customers.push(this.customerForm.value);
+        this.customerForm.reset();
     }
 }
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerPageComponent } from './customer-page/customer-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [
-    CustomerPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        CustomerPageComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
 })
 export class CustomersModule { }
